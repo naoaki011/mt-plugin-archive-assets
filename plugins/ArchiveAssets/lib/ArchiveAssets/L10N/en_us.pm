6394a118226d517d96fcff0ecb0497eaf9cffa30
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-10-07 19:46:29 +0900

    add translate.

---
+++ plugins/ArchiveAssets/lib/ArchiveAssets/L10N/en_us.pm
@@ -0,0 +1,11 @@
+package ArchiveAssets::L10N::en_us;
+
+use strict;
+
+use base 'ArchiveAssets::L10N';
+use vars qw( %Lexicon );
+%Lexicon = (
+
+);
+
+1;
---
+++ plugins/ArchiveAssets/lib/ArchiveAssets/L10N/ja.pm
@@ -0,0 +1,19 @@
+package ArchiveAssets::L10N::ja;
+
+use strict;
+use base 'ArchiveAssets::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+	'Allowing users to upload Archives into their Movable Type.' => '圧縮ファイルをアイテムとして扱う機能をMovable Typeに追加します。',
+	'Insert GoogleAnalytics PageTracker code:' => 'GoogleAnalytics PageTrackerコードを挿入する:',
+	'Extract and Delete Archives:' => '解凍後にアーカイブを削除する:',
+	'Overwrite Extracted Files:' => '解凍されたファイルで上書きする:',
+	'Extract' => 'アーカイブを解凍する',
+	'Tracking this File' => 'このファイルをTrackingする',
+);
+
+1;
+
